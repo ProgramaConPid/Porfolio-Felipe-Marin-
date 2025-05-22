f412ae823b3f8c28e37d1b251973af0eb3c7e242
Felipe Marin <programaconpid@gmail.com>
2025-05-22 13:28:54 -0500

    Nueva seccion en HTML y mas media queries en CSS

---
+++ main.js
@@ -0,0 +1,22 @@
+// Elements
+const hamburguerMenu = document.querySelector("#hamburguer__menu");
+const responsiveNav = document.querySelector(".nav__links");
+const links = document.querySelectorAll(".nav__link")
+
+// Show Menu (Tablet and Mobile)
+hamburguerMenu.addEventListener('click', () => {
+
+  responsiveNav.classList.toggle('active')
+
+})
+
+// Remove class .active from CSS once the user click any link
+links.forEach(link => {
+  link.addEventListener("click", () => {
+
+    if (responsiveNav.classList.contains("active")) {
+      responsiveNav.classList.remove("active")
+    }
+
+  })
+})
