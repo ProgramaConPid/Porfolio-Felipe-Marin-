b1c4a743d744bf824b84c456ca83d06fd69c9cac
Felipe Marin <programaconpid@gmail.com>
2025-06-22 23:44:01 -0500

    Modificaciones en algunas secciones

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+// Loader hide on page load
+window.addEventListener('load', () => {
+  const loader = document.getElementById('loader');
+  if (loader) {
+    loader.style.opacity = '0';
+    setTimeout(() => loader.style.display = 'none', 1500);
+  }
+});
+
 // Elements
 const hamburguerMenu = document.querySelector("#hamburguer__menu");
 const responsiveNav = document.querySelector(".nav__links");
